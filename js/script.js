d6fb2c24d8f9d636c082594b9fa991c48edd584b
Giada92 <giada_meloni@virgilio.it>
2021-05-18 14:50:42 +0200

    Inizio parte vue

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            images: [
+                "img/img_1.jpg",
+                "img/img_2.jpg",
+                "img/img_3.jpg",
+                "img/img_4.jpg",
+                "img/img_5.jpg"
+            ],
+            imageIndex: 0
+        },
+        methods: {
+            nextImg: function() {
+                this.imageIndex++;
+                if(this.imageIndex >= this.images.length) {
+                    this.imageIndex = 0;
+                }
+            },
+            prevImg: function() {
+                this.imageIndex--;
+                if(this.imageIndex <= -1) {
+                    this.imageIndex = this.images.length - 1;
+                }
+            } 
+        }
+    }
+);
